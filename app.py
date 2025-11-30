620a18e25d34c47f9c37724b97e3e75d6d9a505a
David32094 <elprincipe32094@gmail.com>
2025-11-29 21:39:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ import urllib3
 from important_zitado import*
 from byte import*
 
-# --- START:  Gemini AI Integration ---
+# --- START: Gemini AI Integration ---
 GEMINI_API_KEY = "AIzaSyB9TsNdahfnFRhx5iX5wlTuqAaFV6uz4q8"
 GEMINI_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent"
 
